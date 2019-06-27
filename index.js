48fe8561e183b3cc98997867d60b3505311c25be
Vilmos Pásztor <pvilmos93@gmail.com>
2019-06-27 16:16:39 +0200

    modify index

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-let stdin = process.stdin; // lekérjük a bemeneti folyamatot
-stdin.setRawMode(true); // végtelen ciklus
+let stdin = process.stdin;
+stdin.setRawMode(true);
 stdin.resume();
-stdin.setEncoding('utf-8'); // kódolás beállítása
+stdin.setEncoding('utf-8');
 stdin.on('data', (key1) => {
   if (key1 === 'q') {
-    process.exit(); // ezzel a paranccsal lép ki a programból. Fontosn, mert különben nincs kilépés!
-  } else if (key1 === 'a') {
-    // spaceship.spaceshipLeft();
-  } else if (key1 === 'd') {
-    // spaceship.spaceshipRight();
+    process.exit();
   }
 });
 let createBoard = require('./array.js');
@@ -20,20 +16,20 @@ let iteration = 0;
 board[5][2] = 7;
 board[7][5] = 7;
 board[10][16] = 7;
-board[6][2] = 0;
-board[8][5] = 0;
-board[11][16] = 0;
+board[6][2] = 'X';
+board[8][5] = 'X';
+board[11][16] = 'X';
 
 const main = () => {
   setInterval(function () {
     console.clear();
     iteration++;
     asteroid.asteroidLeft(board);
-    asteroid.asteroidRight(board);
+    if (iteration % 2 === 0) {
+      asteroid.asteroidRight(board);
+    }
     createBoard.printMatrix(board);
     console.log('iteration:', iteration);
-    /* if (iteration % 21 === 0) {
-    } */
   }, 200);
 };
 
