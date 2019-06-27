a1a81a66fd34302e05c5dc51c013d3d1af5db2c6
hkingaa <hkingaa@gmail.com>
2019-06-27 16:26:18 +0200

    create mothership movement procedures

---
+++ mothership.js
@@ -0,0 +1,42 @@
+let mothershipDirection = 'right';
+
+const mshipLeft = (mothershipRow) => {
+  if (mothershipDirection === 'left') {
+    mothershipRow.shift();
+    mothershipRow.push(0);
+  }
+};
+
+const fillMothership = (board) => {
+  const mothershipRow = board[0];
+  for (let i = 0; i <= 5; i++) {
+    mothershipRow[i] = 'M';
+  }
+};
+
+const mshipRight = (mothershipRow) => {
+  if (mothershipDirection === 'right') {
+    mothershipRow.unshift(0);
+    mothershipRow.pop();
+  }
+};
+
+const move = (board) => {
+  const mothershipRow = board[0];
+  if (mothershipDirection === 'right') {
+    mshipRight(mothershipRow);
+  } else {
+    mshipLeft(mothershipRow);
+  }
+  if (mothershipRow[19] === 'M') {
+    mothershipDirection = 'left';
+  }
+  if (mothershipRow[0] === 'M') {
+    mothershipDirection = 'right';
+  }
+};
+
+module.exports = {
+  move: move,
+  init: fillMothership
+};
