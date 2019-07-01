6c3c52d62dc6dcb783a6739ab45ba2184fbe19dc
hkingaa <hkingaa@gmail.com>
2019-07-01 15:25:10 +0200

    creating scoreboard top10

--- scoreboard.js
+++ scoreboard.js
@@ -13,6 +13,45 @@ const save = (player, iteration) => {
   console.log(iteration);
 };
 
+const topScores = () => {
+  let matrix = [];
+  fs.readFile('./scoreboard.txt', 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    } else {
+      const lines = data.split('\n');
+      for (let i = 0; i < lines.length; i++) {
+        const parsedLine = lines[i].split(' ');
+        if (parsedLine[0].length > 0 && (typeof parseInt(parsedLine[1])) !== 'NaN') {
+          matrix.push([
+            parsedLine[0],
+            parseInt(parsedLine[1])
+          ]);
+        }
+      }
+
+      let n = matrix.length;
+      for (let i = n - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+          if (matrix[j][1] < matrix[j + 1][1]) {
+            let temp = matrix[j][1];
+            matrix[j][1] = matrix[j + 1][1];
+            matrix[j + 1][1] = temp;
+          }
+        }
+      }
+      const dislpayRowcount = matrix.length > 10 ? 10 : matrix.length;
+      for (let i = 0; i < dislpayRowcount; i++) {
+        console.log(matrix[i][0], matrix[i][1]);
+      }
+      console.log(matrix);
+    }
+  });
+};
+topScores();
+console.log(topScores);
+
 module.exports = {
-  save: save
+  save: save,
+  topScores: topScores
 };
